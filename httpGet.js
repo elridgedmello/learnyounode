328485dfaf789a798d8338f644ac4200dda5f5d9
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-06-24 17:27:02 -0500

    Solution for simple http get

---
+++ httpGet.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function(response) {
+    "use strict";
+
+    if (response) {
+        response.setEncoding('utf8');
+        response.on('data', console.log);
+        response.on('error', console.error);
+    }
+
+});
